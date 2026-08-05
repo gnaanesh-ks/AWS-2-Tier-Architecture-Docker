266b1ddf4ef3cb2b7d54ab608aa83aa992a0d0d4
Gnaanesh KS <ksg.16041996@gmail.com>
2026-08-05 09:34:20 +0200

    Delete terraform directory

--- terraform/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "db_password" {
-  description = "The password for the RDS MySQL database."
-  type        = string
-  sensitive   = true # Mark as sensitive so Terraform doesn't print it in logs
-}
